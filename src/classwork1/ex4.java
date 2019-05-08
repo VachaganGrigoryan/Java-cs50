febf45253ba9ae2a8eaf08cb7e1d072fc5026586
Vachagan Grigoryan <vache.grg40@gmail.com>
2019-05-08 22:35:03 +0400

    add new file

---
+++ src/classwork1/ex4.java
@@ -0,0 +1,35 @@
+package classwork1;
+
+import java.util.Scanner;
+
+public class ex4 {
+    public static void main(String[] args){
+        System.out.println("Enter Triangle a, b, c ");
+        Scanner myObject = new Scanner(System.in);
+
+        int a = myObject.nextInt();
+        int b = myObject.nextInt();
+        int c = myObject.nextInt();
+
+        b = swap(a,a=b);
+
+        if(a>b && a>c)
+            c = swap(a,a=c);
+        else if (b>c)
+            c = swap(b,b=c);
+
+        if (a+b>c)
+            if(a*a+b*b==c*c)
+                System.out.println("Right Triangle");
+            else if (a*a+b*b<c*c)
+                System.out.println("Obtuse Triangle");
+            else
+                System.out.println("Acute Triangle");
+        else
+            System.out.println("No Triangle");
+
+    }
+    public static int swap(int x,int y){
+        return x;
+    }
+}
