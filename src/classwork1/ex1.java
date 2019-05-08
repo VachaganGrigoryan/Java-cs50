b39bbde425bd644881b15f80f50021cf674d13e3
Vachagan Grigoryan <vache.grg40@gmail.com>
2019-05-09 03:11:07 +0400

    add new file

--- src/classwork1/ex1.java
+++ src/classwork1/ex1.java
@@ -4,19 +4,14 @@ public class ex1 {
     public static void main(String[] args) {
 
         int sum = 143;
-
         int[] coin = {50, 25, 10, 5, 1};
 
-        int i = 0;
-
-        while (sum > 0){
-            int item = sum/coin[i];
+        for(int i:coin){
+            int item = sum/i;
             if(item > 0) {
-                System.out.println(item + " * " + coin[i]);
-                sum = sum - item * coin[i];
+                System.out.println(item + " * " + i);
+                sum = sum - item * i;
             }
-            i++;
         }
-
     }
 }
