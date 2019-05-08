d8c9c545695f3f3ef4377eeb8f01cc1d7453af61
Vachagan Grigoryan <vache.grg40@gmail.com>
2019-05-08 21:55:18 +0400

    add new file

---
+++ src/classwork1/ex2.java
@@ -0,0 +1,21 @@
+package classwork1;
+
+public class ex2 {
+    public static void main(String[] args) {
+
+        int n = 10;
+        int n2 = n/2;
+
+        for (int i = 0; i < n2; i++){
+            for (int j = 0; j < n2-i-1;  j++)
+                System.out.print(" ");
+            for (int j = n2-i-1; j < n2;  j++)
+                System.out.print("#");
+            System.out.print(" ");
+            for (int j = n2; j < n2+i+1;  j++)
+                System.out.print("#");
+            System.out.println(" ");
+
+        }
+    }
+}
