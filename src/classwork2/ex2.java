76b45adbc98ce089dc68f73d2a8a72476d5fb352
Vachagan Grigoryan <vache.grg40@gmail.com>
2019-05-10 01:41:45 +0400

    add new file

--- src/classwork2/ex2.java
+++ src/classwork2/ex2.java
@@ -10,10 +10,11 @@ public class ex2 {
         int key = myint.nextInt();
         char[] word = mychar.nextLine().toCharArray();
         String newword = "";
+        key = key%26;
 
         for (char i:word) {
             char c = (char)(i + key);
-            if(c > 'Z'  && !(c >= 'a' && c <= 'z')){
+            if(c > 'Z'  && i >= 'A' && i <= 'Z'){
                 c = (char)(c - 'Z' + 'A' - 1);
             }else if (c > 'z'){
                 c = (char)(c - 'z' + 'a' - 1);
